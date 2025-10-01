67ed2ee32a83e69e733aea614c04eff01916267d
Naseem030583 <137470997+Naseem030583@users.noreply.github.com>
2025-10-01 13:52:16 -0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,9 @@
+# TotalSum variable
+TotalSum = 0
+
+# for loop to iterate from 1 to 50
+for number in range(1, 51):
+    TotalSum += number
+
+# Display TotalSum
+print(f"The sum of numbers from 1 to 50 is: {TotalSum}")
